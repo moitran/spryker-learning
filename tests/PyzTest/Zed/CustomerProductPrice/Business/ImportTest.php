8350225f1780666bbdda330e442e87ad6d9bbf95
moitran <moitran92@gmail.com>
2022-04-07 11:09:29 +0700

    update unit test - add missing cases

--- tests/PyzTest/Zed/CustomerProductPrice/Business/ImportTest.php
+++ tests/PyzTest/Zed/CustomerProductPrice/Business/ImportTest.php
@@ -14,7 +14,7 @@ use Pyz\Zed\CustomerProductPrice\Persistence\PyzCustomerProductQuery;
  */
 class ImportTest extends Unit
 {
-    public function testOutput()
+    public function testImportSuccess()
     {
         // A1 - arrange
         $filePath = dirname(__DIR__) . '/_data/data-import.json';
@@ -27,5 +27,46 @@ class ImportTest extends Unit
         $this->assertTrue($success);
         $this->assertEquals(4, PyzCustomerProductQuery::create()->count());
         $this->assertEquals(7, PyzCustomerProductPriceQuery::create()->count());
+    }
+
+    public function testParsingFileFailed()
+    {
+        // A1 - arrange
+        $filePath = dirname(__DIR__) . '/_data/data-import-invalid-format.json';
+        $importer = new Importer(new CustomerProductPriceEntityManager());
+
+        // A2 - act
+        try {
+            $importer->import($filePath);
+        } catch (\Exception $e) {
+            // A3 - assert
+            $this->assertEquals('Json file invalid', $e->getMessage());
+        }
+    }
+
+    public function testImportFileIsUnread()
+    {
+        // A1 - arrange
+        $filePath = dirname(__DIR__) . '/_data/data-import-not-found.json';
+        $importer = new Importer(new CustomerProductPriceEntityManager());
+
+        // A2 - act
+        $failed = $importer->import($filePath);
+
+        // A3 - assert
+        $this->assertFalse($failed);
+    }
+
+    public function testImportFilePathEmpty()
+    {
+        // A1 - arrange
+        $filePath = '';
+        $importer = new Importer(new CustomerProductPriceEntityManager());
+
+        // A2 - act
+        $failed = $importer->import($filePath);
+
+        // A3 - assert
+        $this->assertFalse($failed);
     }
 }
