64535555a5d3926a18fbc4b4be87ec20e7d3d706
moitran <moitran92@gmail.com>
2022-04-09 14:04:30 +0700

    add events for P&S

---
+++ src/Pyz/Zed/CustomerProductPrice/Dependency/CustomerProductPriceEvents.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pyz\Zed\CustomerProductPrice\Dependency;
+
+/**
+ * Class CustomerProductPriceEvents
+ * @package Pyz\Zed\CustomerProductPrice\Dependency
+ */
+class CustomerProductPriceEvents
+{
+    public const ENTITY_PYZ_CUSTOMER_PRODUCT_PRICE_CREATE = 'Entity.pyz_customer_product_price.create';
+    public const ENTITY_PYZ_CUSTOMER_PRODUCT_PRICE_UPDATE = 'Entity.pyz_customer_product_price.update';
+    public const ENTITY_PYZ_CUSTOMER_PRODUCT_PRICE_DELETE = 'Entity.pyz_customer_product_price.delete';
+}
